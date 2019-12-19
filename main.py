f51c796e0c2238a3d356d6e1b4fffdca077df3a8
MikePolyakov <id2k1149@gmail.com>
2019-12-18 20:38:26 -0500

    rest api hh

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import requests
 import json
+import pprint
 from collections import Counter
 
 
@@ -34,6 +35,7 @@ print(f'найдено {total_found} вакансий')
 print('******************************')
 print('Идет просмотр страницы вакансий, подсчет средней зарплаты и анализ необходимых навыков....')
 print('Пожалуйста, подождите. Это может занять некоторое время....')
+print()
 
 
 count = 0
@@ -41,12 +43,15 @@ salary_count = 0
 salary_average = 0
 # количество страниц для расчетов можно сделать = total_pages
 pages_to_analyse = 1
+print(f'Для ускорения просмотрим вакансии только на {pages_to_analyse} стр...')
+print()
+data = {}
+skill_list = []
 for j in range(pages_to_analyse):
     page = j
     params['page'] = page
     result = vacancy_page(params)
     items = result['items']
-    skill_list = []
     vacancy_with_skill_count = 0
     for i in range(len(items)):
         count += 1
@@ -69,7 +74,7 @@ for j in range(pages_to_analyse):
                 if salary['from'] is not None:
                     salary_count += 1
                     salary_average += salary['from']
-
+print(f'Для анализа было просмотренно только {count} вакансий')
 print('*' * 60)
 print(f'Среди просмотренных вакансий было только {salary_count} с указанием зарплаты')
 print(f'Средняя зарплата = {salary_average//salary_count} руб')
@@ -77,7 +82,7 @@ print('*' * 60)
 print(f'Вакансий с требованиями было {vacancy_with_skill_count}')
 
 print('Часто встречающиеся требования - ')
-short_skill_list = [x for x in skill_list if skill_list.count(x) > 2]
+short_skill_list = [x for x in skill_list if skill_list.count(x) > 1]
 
 skill_rating = Counter(short_skill_list).most_common()
 
@@ -88,7 +93,7 @@ for i in skill_rating:
 requirements = []
 for i in skill_rating:
     share = float("{0:.1f}".format(i[1] * 100 / skill_count))
-    print(f'{i[0]} встречается в {i[1]} вакансиях -> {share}%')
+    print(f'- {i[0]} встречается в {i[1]} вакансиях -> {share}%')
     element = {'name': i[0], 'count': i[1], 'persent': share}
     requirements.append(element)
 
@@ -96,15 +101,10 @@ print('*' * 60)
 output_file = 'saved_data.json'
 print(f'Все результаты будут сохранены в файл {output_file}')
 
-data = []
-e_keywords = {'keywords': key_word}
-data.append(e_keywords)
-e_count = {'count': vacancy_with_skill_count}
-data.append(e_count)
-e_requirements = {'requirements': requirements}
-data.append(e_requirements)
+data['keywords'] = key_word
+data['count'] = vacancy_with_skill_count
+data['requirements'] = requirements
 
-data_json = json.dumps(data)
-
-with open(output_file, 'w', encoding='utf-8') as json_file:
-    json.dump(data_json, json_file)
+pprint.pprint(data)
+with open(output_file, 'w', encoding='utf8') as json_file:
+    json.dump(data, json_file)
